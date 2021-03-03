14296b930a31d046ef735cf60afd18801b789d57
ryohei <ryouheiprograming@gmail.com>
2021-03-03 17:10:03 +0900

    Fix: getter, setterをlomdockを利用しない形に変更

--- getapi/src/main/java/com/getapi/getapi/domain/Item.java
+++ getapi/src/main/java/com/getapi/getapi/domain/Item.java
@@ -18,66 +18,67 @@ import lombok.Setter;
 @Entity
 @Table(name = "items")
 // @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Item implements Serializable{
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Getter
-	@Setter	
-	private Integer id;
-	@Getter
-	@Setter
-	@Column(nullable = false)
-	private String name;
-	@Getter
-	@Setter
-	@Column(nullable = false)
-	private Integer price;
-	@Getter
-	@Setter
-	@Column(nullable = false)
-	private String imgPath;
-}
-
-// public class Item {
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class Item implements Serializable{
+// 	@Id
 // 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+// 	@Getter
+// 	@Setter	
 // 	private Integer id;
+// 	@Getter
+// 	@Setter
 // 	@Column(nullable = false)
 // 	private String name;
+// 	@Getter
+// 	@Setter
 // 	@Column(nullable = false)
 // 	private Integer price;
+// 	@Getter
+// 	@Setter
+// 	@Column(nullable = false)
 // 	private String imgPath;
-
-// 	public long getId() {
-// 			return this.id;
-// 	}
-
-// 	public String getName() {
-// 			return this.name;
-// 	}
-
-// 	public long getPrice() {
-// 		return this.price;
-// 	}
-
-// 	public String getImgPath() {
-// 			return this.imgPath;
-// 	}
-
-// 	public void setId(final Integer id) {
-// 			this.id = id;
-// 	}
-
-// 	public void setName(final String name) {
-// 			this.name = name;
-// 	}
-
-// 	public void setPrice(final Integer price) {
-// 			this.price = price;
-// 	}
-
-// 	public void setImgPath(final String imgPath) {
-// 		this.imgPath = imgPath;
-// 	}
 // }
+
+public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private Integer price;
+	private String imgPath;
+
+	public long getId() {
+			return this.id;
+	}
+
+	public String getName() {
+			return this.name;
+	}
+
+	public long getPrice() {
+		return this.price;
+	}
+
+	public String getImgPath() {
+			return this.imgPath;
+	}
+
+	public void setId(final Integer id) {
+			this.id = id;
+	}
+
+	public void setName(final String name) {
+			this.name = name;
+	}
+
+	public void setPrice(final Integer price) {
+			this.price = price;
+	}
+
+	public void setImgPath(final String imgPath) {
+		this.imgPath = imgPath;
+	}
+}
