9151c4dbf3206ce3b9f2bc5851523ccea136547d
ryohei <ryouheiprograming@gmail.com>
2021-03-06 12:58:14 +0900

    ADD: プロジェクト作成とAPI仕様書の作成

---
+++ finaltraining/src/main/java/com/betterbound/finaltraining/FinaltrainingApplication.java
@@ -0,0 +1,13 @@
+package com.betterbound.finaltraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaltrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaltrainingApplication.class, args);
+	}
+
+}
---
+++ finaltraining/src/test/java/com/betterbound/finaltraining/FinaltrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterbound.finaltraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaltrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
