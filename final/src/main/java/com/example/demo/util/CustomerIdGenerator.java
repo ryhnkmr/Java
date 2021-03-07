50fde7aabae3d2cb3cb7a6b7bb7a4580a16b4184
ryohei <ryouheiprograming@gmail.com>
2021-03-07 22:14:54 +0900

    Add: 顧客番号ランダム生成

--- final/src/main/java/com/example/demo/util/CustomerIdGenerator.java
+++ final/src/main/java/com/example/demo/util/CustomerIdGenerator.java
@@ -9,7 +9,10 @@ import org.hibernate.id.IdentityGenerator;
 public class CustomerIdGenerator extends IdentityGenerator {
   @Override
     public Serializable generate(SharedSessionContractImplementor session, Object obj) {
-        // UUIDを生成し返却する
-        return UUID.randomUUID().toString();
+        // ランダムに大文字のアルファベットを生成
+        char character = Character.toUpperCase((char) Math.floor( Math.random() * 26 + 97 ));
+        // ランダムな4桁の数字を生成
+        Integer num = (int) Math.floor( Math.random() * 10000) - 1;
+        return character + String.format("%04d", num);
     }
 }
